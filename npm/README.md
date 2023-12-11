b4709922354046f338abc12a18541d54845eaa4e
rohannaroni <rohannaroni100@gmail.com>
2023-12-11 14:36:03 +0530

    landing page using bootstrap

---
+++ npm/README.md
@@ -0,0 +1,8 @@
+news api key:
+4485d55d67144a599787476e03bfa459
+
+youtube link
+https://www.youtube.com/@rnx-editz
+
+Instgram link
+https://www.instagram.com/rnxeditzz/
---
+++ npm/script.js
@@ -0,0 +1,12 @@
+function openIt() {
+    const url = 'https://www.youtube.com/@rnx-editz';
+    window.open(url);
+}
+
+
+// in index.html i used onclick=function1 
+
+function function1() { 
+    const url = 'https://www.instagram.com/rnxeditzz/';
+    window.open(url);
+    } 
